a270b2653ae6628b4e7370cfa526b0a67f852c3d
Nana Amoako <nanaamoako202@gmail.com>
2026-03-19 16:04:03 +0000

    Initial Commit

---
+++ HerView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HerView
+//
+//  Created by Nana Amoako on 19/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HerView/HerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerViewApp.swift
+//  HerView
+//
+//  Created by Nana Amoako on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
